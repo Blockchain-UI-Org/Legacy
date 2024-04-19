2e1e0e8f12a61a829895cc28e2130a239679dc3a
Christopher Igbojekwe <christopher.igbojekwe@gmail.com>
2024-04-19 11:25:39 -0600

    modify website text test

--- packages/bui-docs/src/components/landing/getStarted/index.tsx
+++ packages/bui-docs/src/components/landing/getStarted/index.tsx
@@ -1,5 +1,5 @@
 import { useHistory } from "@docusaurus/router";
-import {useColorMode} from "@docusaurus/theme-common"
+import { useColorMode } from "@docusaurus/theme-common";
 import React from "react";
 import { GradientButton } from "../../Button/GradientButton";
 import { Heading } from "../../Heading/Heading";
@@ -7,23 +7,23 @@ import "./GetStarted.scss";
 
 const GetStartedSection = () => {
   const history = useHistory();
-  const {colorMode} = useColorMode()
+  const { colorMode } = useColorMode();
   return (
     <section className="getStarted">
       <div className="container">
         <div className="row ">
           <div className="col-md-5 ">
             <div className="leftSection">
-              <Heading >
-                Create beautiful <span className={`${colorMode}-colormode-text`}>Dapps</span> quickly.
+              <Heading>
+                Create beautiful <span className={`${colorMode}-colormode-text`}>Dapps and Apps</span> quickly.
               </Heading>
               <div className="description">
                 Blockchain UI is a simple, modular and accessible component library designed to help you build web3
                 apps.
               </div>
               <div className="rightSection d-md-none">
-              <img src="/img/mock-up1.svg" alt="iphone_mockup" />
-            </div>
+                <img src="/img/mock-up1.svg" alt="iphone_mockup" />
+              </div>
               <div className="contactUs">Beta release now available.</div>
 
               <GradientButton className="button" onClick={() => history.push("/docs/intro")}>
