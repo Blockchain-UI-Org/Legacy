8faa040b10179eea76fd50906163cd39ea769b9a
Christopher Igbojekwe <christopher.igbojekwe@gmail.com>
2024-04-26 14:23:20 -0600

    test preview config

--- packages/bui-docs/src/components/landing/index.tsx
+++ packages/bui-docs/src/components/landing/index.tsx
@@ -43,7 +43,7 @@ const ImageContainer = styled.div`
   overflow: auto;
   align-content: center;
   align-items: center;
-  margin-top: 250px;
+  margin-top: 150px;
 
   @media screen and (max-width: 768px) {
     margin-top: 0px;
